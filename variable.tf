8f9c062ecc30e13b3c818b3b57f8b98dd9b283e8
Ewambe <ewambe5@gmail.com>
2023-02-18 12:40:46 -0500

    vpc creation

--- variable.tf
+++ variable.tf
@@ -1,15 +1,51 @@
 # environment variables
 variable "region" {
   description = "region to create resources"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "project name"
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "environment"
-  type = string
+  type        = string
+}
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_subnet_az1_cidr" {
+  description = "private subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_subnet_az2_cidr" {
+  description = "private subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "private data subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "private data subnet az2 cidr block"
+  type        = string
 }
